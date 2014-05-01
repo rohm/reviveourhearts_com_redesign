3ad7934fafe0afd1c081826147e7b3c10db2243f
Mark Priestap <mapriestap@gmail.com>
2014-05-01 17:04:59 -0400

    upgrade events carousel to include call to action

--- events.php
+++ events.php
@@ -15,7 +15,7 @@ $bodyCss = "events page page-full-width has-top-social-buttons page-secondary-na
 
 <div class="container">
 	
-	<div class="jumbotron" style="padding-top: 80px; background: #333  url(https://deasb92jiz6fv.cloudfront.net/9b/7242b98c6fe6bda18bccdc61a4ea2b/lightstock-96118-family-bible-study-after-the-thanksgiving-meal--16.jpg); background-size: 100% auto; background-position: center center; background-repeat: no-repeat;">
+	<!-- <div class="jumbotron" style="padding-top: 80px; background: #333  url(https://deasb92jiz6fv.cloudfront.net/9b/7242b98c6fe6bda18bccdc61a4ea2b/lightstock-96118-family-bible-study-after-the-thanksgiving-meal--16.jpg); background-size: 100% auto; background-position: center center; background-repeat: no-repeat;">
 		<h1>Big Huge Ad for Upcoming Event</h1>
 		<div class="video-container">
 			<div class="video">
@@ -23,8 +23,7 @@ $bodyCss = "events page page-full-width has-top-social-buttons page-secondary-na
 					<object width="436" height="331" type="application/x-shockwave-flash" data="/build/flashmediaelement.swf">        
             <param name="movie" value="<?php echo $rohDomain; ?>/static/swf/player.swf" /> 
             <param name="flashvars" value="controls=true&amp;file=/media/connection.mp3" />         
-            <!-- Image fall back for non-HTML5 browser with JavaScript turned off and no Flash player installed -->
-            <img src="http://placehold.it/800x640/text=No+Video+Playback+Capabilities" alt="No video playback capabilities" 
+		<img src="http://placehold.it/800x640/text=No+Video+Playback+Capabilities" alt="No video playback capabilities" 
                 title="No video playback capabilities" />
 	        </object>
 				</video>
@@ -33,8 +32,76 @@ $bodyCss = "events page page-full-width has-top-social-buttons page-secondary-na
 		<nav style="margin-top: 30px;">
 		<a class="btn btn-primary btn-lg">Register Today</a> 
 		</nav>
-	</div>
+	</div> -->
 	
+	<div id="events-carousel-container">
+		<div class="container">
+			<div id="events-carousel" class="carousel slide hero-carousel carousel-fade" data-ride="events-carousel">
+
+				<div class="carousel-indicators hidden-xs">
+					<div class="indicator" data-target="#events-carousel" data-slide-to="0" class="active">
+						<img class="thumbnail img-responsive" src="http://placehold.it/60x60/2980b9/fff&text=1" alt="" />
+					</div>
+					<div class="indicator" data-target="#events-carousel" data-slide-to="1">
+						<img class="thumbnail img-responsive" src="http://placehold.it/60x60/8e44ad/fff&text=2" alt="" />
+					</div>
+					<div class="indicator" data-target="#events-carousel" data-slide-to="2">
+						<img class="thumbnail img-responsive" src="http://placehold.it/60x60/34495e/fff&text=3" alt="" />
+					</div>
+				</div>
+			
+				<div class="carousel-inner">
+          <div class="item active">
+              <img src="http://placehold.it/1200x500/3498db/2980b9" alt="First slide">
+              <div class="carousel-caption">
+                  <h3>
+                      First slide</h3>
+                  <p>
+                      Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+              </div>
+          </div>
+          <div class="item">
+              <img src="http://placehold.it/1200x500/9b59b6/8e44ad" alt="Second slide">
+              <div class="carousel-caption">
+                  <h3>
+                      Second slide</h3>
+                  <p>
+                      Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+              </div>
+          </div>
+          <div class="item">
+              <img src="http://placehold.it/1200x500/34495e/2c3e50" alt="Third slide">
+              <div class="carousel-caption">
+                  <h3>
+                      Third slide</h3>
+                  <p>
+                      Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+              </div>
+          </div>
+      </div>
+			<div>
+			  <a class="left carousel-control" href="#events-carousel" data-slide="prev">
+			    <span class="glyphicon glyphicon-chevron-left"></span>
+			  </a>
+			  <a class="right carousel-control" href="#events-carousel" data-slide="next">
+			    <span class="glyphicon glyphicon-chevron-right"></span>
+			  </a>
+
+			</div>
+			<div class="main-text hidden-xs">
+        <div class="col-md-12 text-center">
+            <h1>
+                True Woman &rsquo;14</h1>
+            <h3>
+                Finding Freedom, Fullness, &amp; Fruitfullness in Christ
+            </h3>
+            <div class="">
+                <a class="btn btn-lg btn-primary" href="http://www.jquery2dotnet.com/">Register Today</a>
+						</div>
+        </div>
+    	</div>
+		</div>
+	</div>
 	
 	
 	<article class="entry">
