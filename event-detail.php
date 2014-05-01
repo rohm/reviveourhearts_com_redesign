556190156c81d48abd74ddf9cfca9d5e4ecb2739
Mark Priestap <mapriestap@gmail.com>
2014-05-01 14:58:44 -0400

    remove bootstrap classes, make the quote module fade

--- event-detail.php
+++ event-detail.php
@@ -39,7 +39,7 @@ $bodyCss = "events page landing landing-event has-top-social-buttons page-second
 
 	  <div class='quotes-inner'>
 
-      <div class="carousel slide" data-ride="carousel" id="quote-carousel">
+      <div class="carousel slide carousel-fade" data-ride="carousel" id="quote-carousel">
         <!-- Bottom Carousel Indicators -->
         <ol class="carousel-indicators">
           <li data-target="#quote-carousel" data-slide-to="0" class="active"></li>
@@ -53,42 +53,38 @@ $bodyCss = "events page landing landing-event has-top-social-buttons page-second
           <!-- Quote 1 -->
           <div class="item active">
             <blockquote>
-              <div class="row">
-                <div class="col-sm-3 text-center">
-                  <img class="img-circle" src="http://wp-desk.com/lovinflat/images/persons/person_1.png" style="width: 100px;height:100px;">
-                </div>
-                <div class="col-sm-9">
-                  <p>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit!</p>
-                  <small>Someone famous</small>
-                </div>
+
+              <div class="photo">
+                <img class="img-circle" src="http://wp-desk.com/lovinflat/images/persons/person_1.png" style="width: 100px;height:100px;">
               </div>
+              <div class="quote-text">
+                <p>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit!</p>
+                <small>Someone famous</small>
+              </div>
+
             </blockquote>
           </div>
           <!-- Quote 2 -->
           <div class="item">
             <blockquote>
-              <div class="row">
-                <div class="col-sm-3 text-center">
-                  <img class="img-circle" src="http://wp-desk.com/lovinflat/images/persons/person_2.png" style="width: 100px;height:100px;">
-                </div>
-                <div class="col-sm-9">
-                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam auctor nec lacus ut tempor. Mauris.</p>
-                  <small>Someone famous</small>
-                </div>
+              <div class="photo">
+                <img class="img-circle" src="http://wp-desk.com/lovinflat/images/persons/person_2.png" style="width: 100px;height:100px;">
+              </div>
+              <div class="quote-text">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam auctor nec lacus ut tempor. Mauris.</p>
+                <small>Someone famous</small>
               </div>
             </blockquote>
           </div>
           <!-- Quote 3 -->
           <div class="item">
             <blockquote>
-              <div class="row">
-                <div class="col-sm-3 text-center">
-                  <img class="img-circle" src="http://wp-desk.com/lovinflat/images/persons/person_3.png" style="width: 100px;height:100px;">
-                </div>
-                <div class="col-sm-9">
-                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut rutrum elit in arcu blandit, eget pretium nisl accumsan. Sed ultricies commodo tortor, eu pretium mauris.</p>
-                  <small>Someone famous</small>
-                </div>
+              <div class="photo">
+                <img class="img-circle" src="http://wp-desk.com/lovinflat/images/persons/person_3.png" style="width: 100px;height:100px;">
+              </div>
+              <div class="quote-text">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut rutrum elit in arcu blandit, eget pretium nisl accumsan. Sed ultricies commodo tortor, eu pretium mauris.</p>
+                <small>Someone famous</small>
               </div>
             </blockquote>
           </div>
