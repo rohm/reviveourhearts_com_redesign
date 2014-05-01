3a8aa47a794bd2301092d4be8d54864b84ca3477
Mark Priestap <mapriestap@gmail.com>
2014-05-01 17:07:41 -0400

    remove containter

--- events.php
+++ events.php
@@ -35,72 +35,65 @@ $bodyCss = "events page page-full-width has-top-social-buttons page-secondary-na
 	</div> -->
 	
 	<div id="events-carousel-container">
-		<div class="container">
-			<div id="events-carousel" class="carousel slide hero-carousel carousel-fade" data-ride="events-carousel">
 
-				<div class="carousel-indicators hidden-xs">
-					<div class="indicator" data-target="#events-carousel" data-slide-to="0" class="active">
-						<img class="thumbnail img-responsive" src="http://placehold.it/60x60/2980b9/fff&text=1" alt="" />
-					</div>
-					<div class="indicator" data-target="#events-carousel" data-slide-to="1">
-						<img class="thumbnail img-responsive" src="http://placehold.it/60x60/8e44ad/fff&text=2" alt="" />
-					</div>
-					<div class="indicator" data-target="#events-carousel" data-slide-to="2">
-						<img class="thumbnail img-responsive" src="http://placehold.it/60x60/34495e/fff&text=3" alt="" />
-					</div>
+		<div id="events-carousel" class="carousel slide hero-carousel carousel-fade" data-ride="events-carousel">
+
+			<div class="carousel-indicators hidden-xs">
+				<div class="indicator" data-target="#events-carousel" data-slide-to="0" class="active">
+					<img class="thumbnail img-responsive" src="http://placehold.it/60x60/0083AD/fff&text=1" alt="" />
 				</div>
-			
-				<div class="carousel-inner">
-          <div class="item active">
-              <img src="http://placehold.it/1200x500/3498db/2980b9" alt="First slide">
-              <div class="carousel-caption">
-                  <h3>
-                      First slide</h3>
-                  <p>
-                      Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-              </div>
-          </div>
-          <div class="item">
-              <img src="http://placehold.it/1200x500/9b59b6/8e44ad" alt="Second slide">
-              <div class="carousel-caption">
-                  <h3>
-                      Second slide</h3>
-                  <p>
-                      Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-              </div>
-          </div>
-          <div class="item">
-              <img src="http://placehold.it/1200x500/34495e/2c3e50" alt="Third slide">
-              <div class="carousel-caption">
-                  <h3>
-                      Third slide</h3>
-                  <p>
-                      Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-              </div>
-          </div>
-      </div>
-			<div>
-			  <a class="left carousel-control" href="#events-carousel" data-slide="prev">
-			    <span class="glyphicon glyphicon-chevron-left"></span>
-			  </a>
-			  <a class="right carousel-control" href="#events-carousel" data-slide="next">
-			    <span class="glyphicon glyphicon-chevron-right"></span>
-			  </a>
-
+				<div class="indicator" data-target="#events-carousel" data-slide-to="1">
+					<img class="thumbnail img-responsive" src="http://placehold.it/60x60/8e44ad/fff&text=2" alt="" />
+				</div>
+				<div class="indicator" data-target="#events-carousel" data-slide-to="2">
+					<img class="thumbnail img-responsive" src="http://placehold.it/60x60/34495e/fff&text=3" alt="" />
+				</div>
 			</div>
-			<div class="main-text hidden-xs">
-        <div class="col-md-12 text-center">
-            <h1>
-                True Woman &rsquo;14</h1>
-            <h3>
-                Finding Freedom, Fullness, &amp; Fruitfullness in Christ
-            </h3>
-            <div class="">
-                <a class="btn btn-lg btn-primary" href="http://www.jquery2dotnet.com/">Register Today</a>
-						</div>
+		
+			<div class="carousel-inner">
+        <div class="item active">
+            <img src="http://placehold.it/1200x500/0083AD/0083AD" alt="First slide">
+            <div class="carousel-caption">
+                <h3>Get Refreshed</h3>
+                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            </div>
         </div>
-    	</div>
+        <div class="item">
+            <img src="http://placehold.it/1200x500/9b59b6/8e44ad" alt="Second slide">
+            <div class="carousel-caption">
+                <h3>Second slide</h3>
+                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            </div>
+        </div>
+        <div class="item">
+            <img src="http://placehold.it/1200x500/34495e/2c3e50" alt="Third slide">
+            <div class="carousel-caption">
+                <h3>Third slide</h3>
+                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            </div>
+        </div>
+    </div>
+		<div>
+		  <a class="left carousel-control" href="#events-carousel" data-slide="prev">
+		    <span class="glyphicon glyphicon-chevron-left"></span>
+		  </a>
+		  <a class="right carousel-control" href="#events-carousel" data-slide="next">
+		    <span class="glyphicon glyphicon-chevron-right"></span>
+		  </a>
+
 		</div>
+		<div class="main-text hidden-xs">
+      <div class="col-md-12 text-center">
+          <h1>True Woman &rsquo;14</h1>
+          <h3>
+              Finding Freedom, Fullness, &amp; Fruitfullness in Christ
+          </h3>
+          <div class="">
+              <a class="btn btn-lg btn-primary" href="http://www.jquery2dotnet.com/">Register Today</a>
+					</div>
+      </div>
+  	</div>
+
 	</div>
 	
 	
