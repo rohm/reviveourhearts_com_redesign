15a64973043948bb09b9d84874a9c3a43ab7aa74
Mark Priestap <mapriestap@gmail.com>
2014-05-09 10:35:13 -0400

    add browse all page, add icons to listed items

---
+++ resources-browse-all.php
@@ -0,0 +1,276 @@
+<?php
+
+$sectionName = "Resources";
+$pageTitle = "Resources";
+$bodyCss = "resources resources-landing page page-default page-narrow-aside";
+
+?>
+
+
+<?php include "_header.php"; ?>
+
+<?php include "_social_buttons_2.php" ?>
+
+
+
+<div class="container">
+	
+	<article class="entry">
+		
+		<header class="entry-header" role="banner">
+			<h1>Resources</h1>
+
+				<form id="search-resources">
+					<div class="input-group search-form">
+						<input type="text" class="form-control" placeholder="Search 5,000 resources">
+						<span class="input-group-btn">
+						<button class="btn btn-primary" type="button">
+							<span class="fa fa-search"></span>
+						</button>
+						</span>
+					</div><!-- /input-group -->
+				</form>
+
+		</header>
+		
+		<section class="main">
+			
+			
+			<div class="clearfix visible-sm visible-md visible-lg"></div>
+			
+			<div id="browse-all">
+				<div id="browse-featured" class="section">
+					<nav>
+						<h3>Essentials</h3>
+						<ul class="bullets">
+							<li><a href="#">Good News</a></li>
+							<li><a href="#">Means of Grace</a></li>
+							<li><a href="#">Heart Revival</a></li>
+							<li><a href="#">True Womanhood</a></li>
+							<li><a href="#">Top Books</a></li>
+							<li><a href="#">Changed Lives</a></li>
+						</ul>
+					</nav>
+					<nav>
+						<h3>Popular</h3>
+						<ul class="bullets">
+							<li><a href="#">Wonder of His Name <i class="fa fa-headphones"></i></a></li>
+							<li><a href="#">Overcoming the Curse of Words <i class="fa fa-headphones"></i></a></li>
+							<li><a href="#">Be Still <i class="fa fa-headphones"></i></a></li>
+							<li><a href="#">Does Jesus Care? (Paul David Tripp) <i class="fa fa-headphones"></i></a></li>
+							<li><a href="#">Marriage Miracle (True Woman video) <i class="fa fa-youtube-play"></i></a></li>
+							<li><a href="#">Joy McClain: A Wife Who Chose to Stay (True Woman video) <i class="fa fa-youtube-play"></i></a></li>
+							<li><a href="#">30-Day Challenges <i class="fa fa-calendar"></i></a></li>
+							<li><a href="#">Brokenness Bookmark: The Heart God Revives <i class="fa fa-file-text-o"></i></a></li>
+							<li><a href="#">The Quiet Place: Daily Devotional Readings <i class="glyphicon glyphicon-book"></i></a></li>
+							<li><a href="#">Lies Women Believe & Truth Bkmk Set <i class="glyphicon glyphicon-book"></i></a></li>
+						</ul>
+					</nav>
+					<nav>
+						<h3>Recommended</h3>
+						<ul class="bullets">
+							<li><a href="#"></a></li>
+							<li><a href="#"></a></li>
+							<li><a href="#"></a></li>
+							<li><a href="#"></a></li>
+							<li><a href="#"></a></li>
+						</ul>
+					</nav>
+					<nav>
+						<h3>30 Day Challenges</h3>
+						<ul class="bullets">
+							<li><a href="#"></a></li>
+							<li><a href="#"></a></li>
+							<li><a href="#"></a></li>
+							<li><a href="#"></a></li>
+							<li><a href="#"></a></li>
+						</ul>
+					</nav>
+					<nav>
+						<h3>Ministry Leader Essentials</h3>
+						<ul class="bullets">
+							<li><a href="#">Ministry Foundations</a></li>
+							<li><a href="#">Biblical Womanhood</a></li>
+							<li><a href="#">Growing as a Leader</a></li>
+							<li><a href="#">Growing Small Groups</a></li>
+							<li><a href="#">Bible Study Principles</a></li>
+							<li><a href="#">Mentoring</a></li>
+							<li><a href="#">Small Group Studies</a></li>
+							<li><a href="#">See all . . .</a></li>
+						</ul>
+					</nav>
+					<nav>
+						<h3>In Spanish</h3>
+						<ul class="bullets">
+							<li><a href="#"></a></li>
+							<li><a href="#"></a></li>
+							<li><a href="#"></a></li>
+							<li><a href="#"></a></li>
+							<li><a href="#"></a></li>
+						</ul>
+					</nav>
+				</div>
+			</div>
+			<div id="essentials">
+				
+				<h2>Essentials</h2>
+				<div class="media-list">
+					<div class="media">
+						<a href="#"><img src="http://placehold.it/360x100" alt="Featured Book Title" class="img-rounded img-responsive"></a>
+						<div class="media-body">
+							<h4><a href="#">The Gospel</a></h4>
+							<p>We all have an insatiable appetite for good news. Here&rsquo;s the best you&rsquo;ll ever read!</p>
+						</div>
+					</div>
+					<div class="media">
+						<a href="#"><img src="http://placehold.it/360x100" alt="Featured Book Title" class="img-rounded img-responsive"></a>
+						<div class="media-body">
+							<h4><a href="#">Means of Grace</a></h4>
+							<p>Think of these spiritual disciplines as means of grace in your life—&rsquo;cause they will be!</p>
+							<nav>
+								<a id="the-word" href="https://www.reviveourhearts.com/resource-library/30-day-challenges/30-day-bible-reading/" class="essential-link"><i></i>The Word</a>
+						</div>
+					</div>
+					<div class="media">
+						<a href="#"><img src="http://placehold.it/360x100" alt="Featured Book Title" class="img-rounded img-responsive"></a>
+						<div class="media-body">
+							<h4><a href="#">Heart Revival</a></h4>
+							<p>Revival . . . a complete transformation — It can happen in your heart, in your home, in your church, and in your world.</p>
+						</div>
+					</div>
+					<div class="clearfix visible-sm visible-md visible-lg"></div>
+					<div class="media">
+						<a href="#"><img src="http://placehold.it/360x100" alt="Featured Book Title" class="img-rounded img-responsive"></a>
+						<div class="media-body">
+							<h4><a href="#">True Womanhood</a></h4>
+							<p>Discover, embrace, and delight in God&rsquo;s design and mission for your life!</p>
+						</div>
+					</div>
+					<div class="media">
+						<a href="#"><img src="http://placehold.it/360x100" alt="Featured Book Title" class="img-rounded img-responsive"></a>
+						<div class="media-body">
+							<h4><a href="#">Top Books</a></h4>
+							<p>After you&rsquo;ve spent time in the Word of God (the only “must read” book we believe in around here), below are a few of our top book recommendations.</p>
+						</div>
+					</div>
+					<div class="media">
+						<a href="#"><img src="http://placehold.it/360x100" alt="Featured Book Title" class="img-rounded img-responsive"></a>
+						<div class="media-body">
+							<h4><a href="#">Changed Lives</a></h4>
+							<p>Want a “pick-me-up”? Read, watch, and listen to the stories of women whose lives have been transformed from the inside out.</p>
+						</div>
+					</div>
+				</div><!-- Media List -->
+			</div>
+			
+			<div id="i-am-a">
+				
+				<h2>I Am A&nbsp;.&nbsp;.&nbsp;.</h2>
+				<div class="media-list">
+					<div class="media">
+						<a href="#"><img src="http://placehold.it/360x100" alt="Featured Book Title" class="img-rounded img-responsive"></a>
+						<div class="media-body">
+							<h4><a href="#">Mother</a></h4>
+							<!-- <p>Raising children and training them in godliness can be a difficult task in society today. We desire these resources to be a source of equipping in your critical role as a mother.</p> -->
+						</div>
+					</div>
+					<div class="media">
+						<a href="#"><img src="http://placehold.it/360x100" alt="Featured Book Title" class="img-rounded img-responsive"></a>
+						<div class="media-body">
+							<h4><a href="#">Teenager</a></h4>
+							<!-- <p>Following Christ as a young woman in high school or college can be tough in a complex, media-driven environment. We hope to encourage you with resources to help navigate the world around you.</p> -->
+						</div>
+					</div>
+					<div class="media">
+						<a href="#"><img src="http://placehold.it/360x100" alt="Featured Book Title" class="img-rounded img-responsive"></a>
+						<div class="media-body">
+							<h4><a href="#">Single Woman</a></h4>
+							<!-- <p>Whether you have never been married, or find yourself single again, we trust these resources will encourage you to make the most of this season where the Lord has you.</p> -->
+						</div>
+					</div>
+					<div class="clearfix visible-sm visible-md visible-lg"></div>
+					<div class="media">
+						<a href="#"><img src="http://placehold.it/360x100" alt="Featured Book Title" class="img-rounded img-responsive"></a>
+						<div class="media-body">
+							<h4><a href="#">Married Woman</a></h4>
+							<!-- <p>Regardless of how easy or difficult your marriage may seem, the Word of God offers practical wisdom for how to live faithfully in this relationship.</p> -->
+						</div>
+					</div>
+					<div class="media">
+						<a href="#"><img src="http://placehold.it/360x100" alt="Featured Book Title" class="img-rounded img-responsive"></a>
+						<div class="media-body">
+							<h4><a href="#">Mentor</a></h4>
+							<!-- <p>The impact of godly mentors cannot be overstated, and the mandate to train younger women in Scripture is clear. We are happy to offer resources that can help you in that ministry.</p> -->
+						</div>
+					</div>
+					<div class="media">
+						<a href="#"><img src="http://placehold.it/360x100" alt="Featured Book Title" class="img-rounded img-responsive"></a>
+						<div class="media-body">
+							<h4><a href="#">Women&rsquo;s Ministry Leader</a></h4>
+							<!-- <p>Working in women&rsquo;s ministry is a battle, and the demands and stakes are high. We want to encourage you to press on, for the reward of changed lives is worth the cost and effort it takes!</p> -->
+						</div>
+					</div>
+					<div class="clearfix visible-sm visible-md visible-lg"></div>
+					<div class="media">
+						<a href="#"><img src="http://placehold.it/360x100" alt="Featured Book Title" class="img-rounded img-responsive"></a>
+						<div class="media-body">
+							<h4><a href="#">Empty Nester</a></h4>
+							<!-- <p>An empty house again can feel strange, but can also provide many opportunities. We want to help you during this season to make the most of these years and to find joy and fulfillment.</p> -->
+						</div>
+					</div>
+					<div class="media">
+						<a href="#"><img src="http://placehold.it/360x100" alt="Featured Book Title" class="img-rounded img-responsive"></a>
+						<div class="media-body">
+							<h4><a href="#">Hurting Women</a></h4>
+							<!-- <p>When life gets difficult, we often struggle to maintain perspective and hope. We trust these resources will point you back to God&rsquo;s Word for stability, assurance, grace, and hope.</p> -->
+						</div>
+					</div>
+					<div class="media">
+						<a href="#"><img src="http://placehold.it/360x100" alt="Featured Book Title" class="img-rounded img-responsive"></a>
+						<div class="media-body">
+							<h4><a href="#">Student of the Word</a></h4>
+							<!-- <p>Do you desire to dig deep into the Word of God? As you seek Him in the pages of His Word, we believe your study will yield abundant fruit.</p> -->
+						</div>
+					</div>
+					<div class="clearfix visible-sm visible-md visible-lg"></div>
+				</div><!-- Media List -->
+			</div>
+
+		</section>
+		
+		<aside>
+			<h3>Featured</h3>
+			<ul class="nav nav-stacked">
+				<li><a href="#">Essentials</a></li>
+				<li><a href="#">Popular</a></li>
+				<li><a href="#">Recommended</a></li>
+				<li><a href="#">Challenges</a></li>
+				<li><a href="#">Ministry Leaders</a></li>
+				
+				<li><a href="#">Spanish</a></li>
+			</ul>
+			<h3>Category</h3>
+			<ul class="nav nav-stacked">
+				<li><a href="#">Articles</a></li>
+				<li><a href="#">Programs</a></li>
+				<li><a href="#">Videos</a></li>
+				<li><a href="#">Wallpapers</a></li>
+				<li><a href="#">FAQs</a></li>
+			</ul>
+			<h3>Topic</h3>
+			<ul class="nav nav-stacked">
+				<li><a href="#">Bible &amp; Theology</a></li>
+				<li><a href="#">Christian Living</a></li>
+				<li><a href="#">Home &amp; Family</a></li>
+				<li><a href="#">Life Issues</a></li>
+				<li><a href="#">Relationships</a></li>
+				
+				<li><a href="#">All Topics. . .</a></li>
+			</ul>
+		</aside>
+		
+		
+	</article>
+</div>
+
+<?php include "_footer.php"; ?>
